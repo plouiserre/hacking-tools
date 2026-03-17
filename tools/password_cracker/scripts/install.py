f3eb0085f94fed829e77551e3322c4536851760d
plouiserre <plouiserre@gmail.com>
2026-03-17 15:18:41 +0100

    install .py did

---
+++ tools/password_cracker/scripts/install.py
@@ -0,0 +1,21 @@
+import os
+from pathlib import Path
+import subprocess
+
+print("---------------------------CD START---------------------------")
+print(__file__)
+os.chdir(Path(__file__).parent)
+print(os.getcwd())  # Affiche le répertoire courant
+dir_path = os.path.dirname(os.path.realpath(__file__))
+print("check dir_path "+dir_path)
+os.chdir(Path(dir_path).parent)
+print(os.getcwd())  # Affiche le répertoire courant
+print("---------------------------CD END---------------------------")
+
+print("---------------------------VENV START---------------------------")
+subprocess.call(["python ","-m", "venv", ".venv"])
+print("---------------------------VENV END---------------------------")
+
+print("---------------------------PIP INSTALL -E .---------------------------")
+subprocess.call(["pip", "install", "-e", "."])
+print("---------------------------PIP INSTALL -E .---------------------------")
